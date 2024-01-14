ae8f618c4372d0f7099e6025c66cddd5248bea23
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-15 01:14:34 +0400

    middleware edited

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
-import { auth } from "./auth";
+import NextAuth from "next-auth";
+
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req)=>{
     const isLoggedIn = !!req.auth;
