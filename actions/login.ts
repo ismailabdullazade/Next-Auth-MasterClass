77a51a8ae05f8752194217ab2a23b71d25dc4f51
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-15 13:31:23 +0400

    login

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,8 @@
 "use server";
-
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import * as z from "zod";
 
 
@@ -11,5 +13,23 @@ export const login = async (values:z.infer<typeof LoginSchema>) => {
         return {error:"Invalid fields"}
     }
 
-    return { success:"Email Sent"}
+    const {email,password} = validatedFields.data;
+
+    try {
+        await signIn("credentials",{
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch(error.type){
+                case "CredentialsSignin":
+                    return { error:"Invalid credentials!"}
+                default:
+                    return {error:"Something went wrong!"}
+            }
+        }
+    }
+    
 }
