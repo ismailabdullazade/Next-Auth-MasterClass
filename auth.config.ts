9743026f24fc2ea02ed3c3c5d38932d205f7349a
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-15 13:33:36 +0400

    auth config

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,30 @@
-import GitHub from "next-auth/providers/github"
 
-import type { NextAuthConfig } from "next-auth"
+import bcrypt from "bcrypt";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials){
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if(validatedFields.success){
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if(!user || !user.password){
+            return null;
+          }
+
+          const passwordMatch = await bcrypt.compare(password,user.password);
+
+          if(passwordMatch) return user;
+        }
+        return null;
+      }
+    })
+  ],
 } satisfies NextAuthConfig
