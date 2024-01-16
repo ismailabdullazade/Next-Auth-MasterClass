04cfd57ed000f44fea37ce7eb38a1f1ee0397952
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-16 13:42:29 +0400

    jwt&session

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -3,7 +3,9 @@ import { auth, signOut } from "@/auth"
 
 const SettingsPage = async () => {
 
-    const session = await auth();
+  const session = await auth();
+
+  
   return (
     <div>
         {JSON.stringify(session)}
