f2c65ba7a76f39d6d26a87777b5698925a82b6cc
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-16 19:40:22 +0400

    bug fix #type

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -22,8 +22,15 @@ import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
+import { useSearchParams } from "next/navigation";
 
 export const LoginForm = () => {
+
+  const searchParams = useSearchParams();
+
+  const urlError = searchParams.get("error") === "OAuthAccountNotLinked"?
+    "Email already in use with different provider!"
+    : "";
 
   const [isPending,startTransition] = useTransition();
   const [error,setError] = useState<string | undefined>("");
@@ -45,7 +52,8 @@ export const LoginForm = () => {
       login(values)
       .then(data=>{
         setError(data?.error);
-        setSuccess(data?.success);
+        // TODO: Add when we will have 2Factor
+        // setSuccess(data?.success);
       })
     })
   }
@@ -99,7 +107,7 @@ export const LoginForm = () => {
               )}
               />
             </div>
-            <FormError message={error} />
+            <FormError message={error || urlError} />
             <FormSuccess message={success} />
             <Button disabled={isPending} type="submit" className="w-full">Login</Button>
           </form>
