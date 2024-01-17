7755b7c5709acbb9afb3be372734d7f016834fd6
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-17 17:42:20 +0400

    reset password

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -23,6 +23,7 @@ import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export const LoginForm = () => {
 
@@ -102,6 +103,11 @@ export const LoginForm = () => {
                     disabled={isPending}
                     />
                   </FormControl>
+                  <Button size="sm" variant="link" asChild className="px-0 font-normal">
+                    <Link href="/auth/reset">
+                      Forgot Password ?
+                    </Link>
+                  </Button>
                   <FormMessage/>
                 </FormItem>
               )}
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,19 +1,37 @@
 "use client"
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { CardWrapper } from "./card-wrapper"
 import {BeatLoader} from "react-spinners";
 import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "../form-success";
+import { FormError } from "../form-error";
 
 export const NewVerificationForm = () => {
+    const [error,setError] = useState<string | undefined>();
+    const [success,setSuccess] = useState<string | undefined>();
 
     const searchParams = useSearchParams();
 
     const token = searchParams.get("token");
 
     const onSubmit = useCallback(()=>{
-        console.log(token)
-    },[token]);
+        if(success || error) return;
+        if(!token) {
+            setError("Missing token!");
+            return;
+        }
+
+        newVerification(token)
+          .then((data) => {
+            setSuccess(data.success);
+            setError(data.error);
+          })
+          .catch(()=>{
+            setError("Something went wrong")
+          })
+    },[token,success,error]);
 
     useEffect(()=>{
         onSubmit();
@@ -26,7 +44,14 @@ export const NewVerificationForm = () => {
           backButtonHref="/auth/login"
         >
             <div className="flex items-center w-full justify-center">
-                <BeatLoader/>
+                {!success && !error && (
+                    <BeatLoader/>
+                )}
+                <FormSuccess message={success}/>
+                
+                {!success && (
+                    <FormError message={error}/>
+                )}
             </div>
             
         </CardWrapper>
