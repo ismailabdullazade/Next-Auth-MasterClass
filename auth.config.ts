adae6e441a2b74d1590d7214b583ef95d0dcb52b
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-25 20:51:36 +0400

    auth config

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,9 @@
 
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 
