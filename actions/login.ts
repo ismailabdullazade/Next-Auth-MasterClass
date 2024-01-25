bbdfb7d6db775d96561a9b1aaad0403645947c07
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-25 20:50:54 +0400

    login

--- actions/login.ts
+++ actions/login.ts
@@ -14,7 +14,10 @@ import { AuthError } from "next-auth";
 import * as z from "zod";
 
 
-export const login = async (values:z.infer<typeof LoginSchema>) => {
+export const login = async (
+    values:z.infer<typeof LoginSchema>,
+    callbackUrl?:string | null, 
+    ) => {
     const validatedFields = LoginSchema.safeParse(values);
 
     if(!validatedFields.success) {
@@ -93,7 +96,7 @@ export const login = async (values:z.infer<typeof LoginSchema>) => {
         await signIn("credentials",{
             email,
             password,
-            redirectTo: DEFAULT_LOGIN_REDIRECT,
+            redirectTo: callbackUrl || DEFAULT_LOGIN_REDIRECT,
         })
     } catch (error) {
         if(error instanceof AuthError){
