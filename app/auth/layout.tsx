207a51c9edc2ab6a603c2053feab4ff89aace2f8
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-27 14:08:24 +0400

    layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,7 @@
 
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className='h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+    <div className='flex items-center justify-center'>
         {children}
     </div>
   )
