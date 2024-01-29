16495d351264d770c3b54658490002a73a544baa
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-29 12:20:51 +0400

    added domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email:string,token:string) => {
     await resend.emails.send({
-        from:"onboarding@resend.dev",
+        from:"mail@authentication-masterclass-ismail.live",
         to:email,
         subject: "2FA Code",
         html: `<p>Your 2FA code :${token}</p>`
@@ -17,7 +17,7 @@ export const sendPasswordResetEmail = async (email:string,token:string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`
 
     await resend.emails.send({
-        from:"onboarding@resend.dev",
+        from:"mail@authentication-masterclass-ismail.live",
         to:email,
         subject:"Reset your password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
@@ -29,7 +29,7 @@ export const sendVerificationEmail = async (email:string,token:string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from:"onboarding@resend.dev",
+        from:"mail@authentication-masterclass-ismail.live",
         to:email,
         subject:"Confirm your email",
         html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
